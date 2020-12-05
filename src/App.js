d27742d3142b90068ac5e6065034d604869ab244
Adam Skoog <adamskoog@outlook.com>
2020-12-05 07:23:03 -0600

    Remove redux store setup - incorrect branch.

--- src/App.js
+++ src/App.js
@@ -2,20 +2,12 @@ import React from 'react';
 import './App.css';
 
 import Main from './components/Main';
-import { createStore, applyMiddleware } from 'redux';
-import allReducers from './context/reducers';
-import { Provider } from 'react-redux';
-import thunk from 'redux-thunk';
 
 function App() {
 
-    const store = createStore(allReducers, applyMiddleware(thunk));
-
     return (
         <div className="App">
-            <Provider store={store}>
-                <Main />
-            </Provider>
+            <Main />
         </div>
     ); 
 }

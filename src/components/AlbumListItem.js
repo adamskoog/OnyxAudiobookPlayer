13face113bab93fd298f60ed1083f9e16643f64b
Adam Skoog <adamskoog@outlook.com>
2020-12-05 15:10:25 -0600

    Add lazy property to image elements. This will avoid loading all images immediately upon load of the page. Fixes #4.

--- src/components/AlbumListItem.js
+++ src/components/AlbumListItem.js
@@ -7,7 +7,7 @@ function AlbumItem(props) {
     return (
         <Col xs={6} sm={6} md={4} xl={3} className="mb-3 album-item">
             <Link to={`/album/${props.albumInfo.ratingKey}`}>
-                <img src={PlexRequest.getThumbnailUrl(props.baseUrl, props.albumInfo.thumb, { "X-Plex-Token": props.userInfo.authToken })} alt="Album Cover"/>
+                <img src={PlexRequest.getThumbnailUrl(props.baseUrl, props.albumInfo.thumb, { "X-Plex-Token": props.userInfo.authToken })} alt="Album Cover" loading="lazy" />
                 <div className="mt-1">{props.albumInfo.title}</div>
                 <div>{props.albumInfo.parentTitle}</div>
             </Link>

7b565774f041101ca19aab88fbcd674f6a5bac46
Adam Skoog <adamskoog@outlook.com>
2021-01-17 09:56:26 -0600

    Really basic horizontal scroll layout.

--- src/components/home/Hub.js
+++ src/components/home/Hub.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import LibraryItem from '../library/LibraryItem';
 
+//https://webdevtrick.com/horizontal-scroll-navigation/
 function Hub(props) {
     return (
         <React.Fragment>
-        <div>{props.title}</div>
-        <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 gap-4">            
-            {props.items.map((item) => (
-                <LibraryItem key={item.ratingKey} baseUrl={props.baseUrl} userInfo={props.userInfo} albumInfo={item} />
-            ))}
+        <div className="font-bold mb-2">{props.title}</div>
+        <div className="w-screen overflow-x-scroll overflow-y-hidden mb-6">
+            <div className="flex flex-row w-max gap-4">            
+                {props.items.map((item) => (
+                    <LibraryItem key={item.ratingKey} baseUrl={props.baseUrl} userInfo={props.userInfo} albumInfo={item} />
+                ))}
+            </div>
         </div>
         </React.Fragment>
     ); 

b0285dc13df1ec99ca10c24b0c221d82d4dda064
Adam Skoog <adamskoog@outlook.com>
2022-02-26 09:14:34 -0600

    Refactor loader to redux hooks and styled components.

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -1,32 +1,54 @@
 
-import React, { useEffect, useRef } from 'react';
-import { connect } from 'react-redux';
+import React from 'react';
+import styled from 'styled-components';
 
-const mapStateToProps = state => {
-    return {
-        isLoading: state.application.isLoading,
-        applicationState: state.application.applicationState
-    };
+import { useSelector } from 'react-redux'
+
+const isLoading = (loading, state) => {
+    // TODO: we should utilize the state to determine when
+    // this is shown.
+    if (loading)
+        return true;
+    return false;
 };
 
-function ConnectedLoader({ isLoading, applicationState }) {
+const LoaderContainer = styled.div`
+    display: ${props => (isLoading(props.isLoading, props.applicationState)) ? 'block' : 'none'};
+    z-index: 50;
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    background-color: rgba(31, 41, 55, 1)
+`;
 
-    const containerRef = useRef(null);
+const Spinner = styled.div`
+    margin: auto;
+    margin-top: 12rem;
+    
+    width: 50px;
+    height: 50px;
+    border: 3px solid rgba(255,255,255,.3);
+    border-radius: 50%;
+    border-top-color: #fff;
+    animation: spin 1s ease-in-out infinite;
 
-    useEffect(() => {
-        if (isLoading) {
-            containerRef.current.classList.replace("hidden", "block");
-        } else {
-            containerRef.current.classList.replace("block", "hidden");
-        }
-    }, [isLoading, applicationState]);
+    @keyframes spin {
+        to { -webkit-transform: rotate(360deg); }
+    }
+`;
+
+const Loader = () => {
+
+    const isLoading = useSelector(state => state.application.isLoading);
+    const applicationState = useSelector(state => state.application.applicationState);
 
     return (
-        <div ref={containerRef} className={"bg-gray-800 absolute z-50 top-0 bottom-0 left-0 right-0 block"}>
-            <div className={"css-loader block m-auto mt-48"} role="status"></div>
-        </div>
+        <LoaderContainer isLoading={isLoading} applicationState={applicationState}>
+            <Spinner role="status" />
+        </LoaderContainer>
     ); 
 }
 
-const Loader = connect(mapStateToProps)(ConnectedLoader);
 export default Loader;

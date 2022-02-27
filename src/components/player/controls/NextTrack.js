dafefa31196c5002a0bcf98dab616162531084b5
Adam Skoog <adamskoog@outlook.com>
2022-02-27 08:06:20 -0600

    Begin refactor of player buttons.

--- src/components/player/controls/NextTrack.js
+++ src/components/player/controls/NextTrack.js
@@ -1,43 +1,41 @@
 
 import React from 'react';
-import { connect } from 'react-redux';
+import styled from 'styled-components';
+import { useSelector, useDispatch } from 'react-redux';
 
-import * as playQueueActions from "../../../context/actions/playQueueActions";
+import { nextTrackInQueue } from "../../../context/actions/playQueueActions";
+import { ReactComponent as NextTrackSvg } from '../../../assets/playerNextTrack.svg';
 
-const mapStateToProps = state => {
-    return { 
-        queueIndex: state.playQueue.index,
-        queue: state.playQueue.queue
-    };
+// TODO: refactor to shared
+const PlayerSmallButton = styled.button`
+    margin-left: 0.5rem;
+    margin-right: 0.5rem;
+
+    font-size: 1.5rem;
+    line-height: 2rem;
+`;
+
+const hasNextTrack = (queueIndex, queue) => {       
+    let newTrackIndex = queueIndex + 1;
+    if (newTrackIndex < queue.length) {
+        return true;
+    }
+    return false;
 };
 
- const mapDispatchToProps = dispatch => {
-    return {
-        nextTrackInQueue: () => dispatch(playQueueActions.nextTrackInQueue())
-    };
-};
+const NexTrackControl = () => {
+    const dispatch = useDispatch();
 
-function ConnectedNexTrackControl(props) {
-
-    const hasNextTrack = () => {
-        let newTrackIndex = props.queueIndex + 1;
-        if (newTrackIndex < props.queue.length) {
-            return true;
-        }
-        return false;
-    };
+    const queueIndex = useSelector(state => state.playQueue.index);
+    const queue = useSelector(state => state.playQueue.queue);
 
     return (
-        <React.Fragment>
-            <button className="text-2xl mx-2 items-center focus:outline-none" type="button" disabled={!hasNextTrack()} onClick={() => props.nextTrackInQueue()}>
-            <svg width="1em" height="1em" viewBox="0 0 16 16" className="bi bi-skip-end-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                <path fillRule="evenodd" d="M12 3.5a.5.5 0 0 1 .5.5v8a.5.5 0 0 1-1 0V4a.5.5 0 0 1 .5-.5z"/>
-                <path d="M11.596 8.697l-6.363 3.692c-.54.313-1.233-.066-1.233-.697V4.308c0-.63.692-1.01 1.233-.696l6.363 3.692a.802.802 0 0 1 0 1.393z"/>
-            </svg>
-            </button>
-        </React.Fragment>
+        <>
+            <PlayerSmallButton disabled={!hasNextTrack(queueIndex, queue)} onClick={() => dispatch(nextTrackInQueue())}>
+                <NextTrackSvg />
+            </PlayerSmallButton>
+        </>
     ); 
 }
 
-const NexTrackControl = connect(mapStateToProps, mapDispatchToProps)(ConnectedNexTrackControl);
 export default NexTrackControl;
--- src/components/player/controls/SkipBack.js
+++ src/components/player/controls/SkipBack.js
@@ -1,31 +1,30 @@
 import React from 'react';
-import { connect } from 'react-redux';
+import styled from 'styled-components';
 
-const mapStateToProps = state => {
-    return { 
-        playState: state.player.mode
-    };
-};
+import { ReactComponent as SkipBackSvg } from '../../../assets/playerSkipBack.svg';
 
-function ConnectedSkipBackControl(props) {
+// TODO: refactor to shared
+const PlayerSmallButton = styled.button`
+    margin-left: 0.5rem;
+    margin-right: 0.5rem;
 
+    font-size: 1.5rem;
+    line-height: 2rem;
+`;
+
+const SkipBackControl = (props) => {
+
+    // TODO: make a setting and pull from state.
     const SKIP_TIME = 10;
     const skipBackward = () => {
         props.skipBackward(SKIP_TIME);
     };
 
     return (
-        <React.Fragment>
-            <button className="text-2xl mx-2 items-center focus:outline-none" type="button" onClick={() => skipBackward()}>
-                <svg width="1em" height="1em" viewBox="0 0 16 16" className="bi bi-skip-backward-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                    <path fillRule="evenodd" d="M.5 3.5A.5.5 0 0 0 0 4v8a.5.5 0 0 0 1 0V4a.5.5 0 0 0-.5-.5z"/>
-                    <path d="M.904 8.697l6.363 3.692c.54.313 1.233-.066 1.233-.697V4.308c0-.63-.692-1.01-1.233-.696L.904 7.304a.802.802 0 0 0 0 1.393z"/>
-                    <path d="M8.404 8.697l6.363 3.692c.54.313 1.233-.066 1.233-.697V4.308c0-.63-.693-1.01-1.233-.696L8.404 7.304a.802.802 0 0 0 0 1.393z"/>
-                </svg>
-            </button>
-        </React.Fragment>
+        <PlayerSmallButton onClick={() => skipBackward()}>
+            <SkipBackSvg />
+        </PlayerSmallButton>
     ); 
 }
 
-const SkipBackControl = connect(mapStateToProps)(ConnectedSkipBackControl);
 export default SkipBackControl;

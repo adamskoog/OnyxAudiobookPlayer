f4f172601ba2c5bcafbd5aecd9e03743f8f58943
Adam Skoog <adamskoog@outlook.com>
2022-03-06 13:47:59 -0600

    Refactoring auth and settings.

--- src/plex/Authentication.js
+++ src/plex/Authentication.js
@@ -1,9 +1,10 @@
 import PlexApi from './Api';
+import { SETTINGS_KEYS, loadSettingFromStorage, saveSettingToStorage, removeSettingFromStorage } from '../utility/settings';
 
 // Action = GET_TOKEN
 export const getAuthTokenFromStorage = () => {
     // Attempt to get the token from browser local storage.
-    let token = localStorage.getItem("authToken");
+    let token = loadSettingFromStorage(SETTINGS_KEYS.token);
     if (token && token !== "")
         return token;
     return null;
@@ -16,14 +17,14 @@ export const validateToken = async (token) => {
     const userInfo = await PlexApi.checkToken(token);
 
     if (userInfo.message) {
-        localStorage.removeItem("authToken");
+        logout();
         return { message: userInfo.message };
     }
     return { user: userInfo };
 }
 
 export const getAuthenticationId = () => {
-    let authId = localStorage.getItem("login_redirect_id");
+    let authId = loadSettingFromStorage(SETTINGS_KEYS.loginRedirectId);
     if (authId && authId !== "")
         return authId;
     return null;
@@ -32,20 +33,21 @@ export const getAuthenticationId = () => {
 export const prepareLoginRequest = async () => {
     const redirectInfo = await PlexApi.signIn();
 
-    localStorage.setItem("login_redirect_id", redirectInfo.id);
+    saveSettingToStorage(SETTINGS_KEYS.loginRedirectId, redirectInfo.id);
+
     return { url: redirectInfo.redirectUrl };
 };
 
 export const validateAuthId = async (authId) => {
     const regInfo = await PlexApi.validatePin(authId);
 
-    localStorage.removeItem("login_redirect_id");
-    localStorage.setItem("authToken", regInfo.authToken);
+    removeSettingFromStorage(SETTINGS_KEYS.loginRedirectId);
+    saveSettingToStorage(SETTINGS_KEYS.authToken, regInfo.authToken);
 
     return { token: regInfo.authToken };
 };
 
 export const logout = () => {
     // Might need to do more here, clear settings??
-    localStorage.removeItem("authToken");
+    removeSettingFromStorage(SETTINGS_KEYS.token);
 }
--- src/utility/settings.js
+++ src/utility/settings.js
@@ -1,6 +1,8 @@
 export const SETTINGS_KEYS = {
     serverId: 'settings_serverIdentifier',
-    libraryId: 'settings_library'
+    libraryId: 'settings_library',
+    token: 'authToken',
+    loginRedirectId: 'login_redirect_id'
 };
 
 export const loadSettingsFromStorage = () => {
@@ -12,6 +14,14 @@ export const loadSettingsFromStorage = () => {
     return settings;
 }
 
+export const loadSettingFromStorage = (key) => {
+    return localStorage.getItem(key);
+}
+
 export const saveSettingToStorage = (key, value) => {
     localStorage.setItem(key, value);    
+}
+
+export const removeSettingFromStorage = (key) => {
+    localStorage.removeItem(key);
 }

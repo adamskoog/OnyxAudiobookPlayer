677fe66fbf92f23cfd48e731f4deb9a610d3f0f0
Adam Skoog <adamskoog@outlook.com>
2022-03-13 17:19:42 -0500

    Add toggle switch for changing theme.

--- src/components/Settings/index.js
+++ src/components/Settings/index.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Switch } from '@headlessui/react';
 import styled from 'styled-components';
 
 import { useSelector, useDispatch } from 'react-redux'
@@ -7,6 +8,7 @@ import { setServerSetting, setLibrarySetting } from "../../context/actions/setti
 import Subheader from '../Header/Subheader';
 import { ScrollContent  } from '../util/container';
 import * as Colors from '../util/colors';
+import { SrOnly } from '../util/common';
 
 // Original tailwind styles.
 /* <select className="mt-2 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"> */
@@ -23,6 +25,36 @@ const SelectControl = styled.select`
     background-color: ${Colors.LIGHT_SETTINGS_CONTROL_BG};
     border: solid 1px ${Colors.LIGHT_SETTINGS_CONTROL_BORDER};
     border-radius: 0.375rem;
+    
+`;
+
+const SwitchContainer = styled.div`
+    margin: 0.5rem 0;
+    .switch-item {    
+        position: relative;
+        display: inline-flex;
+        align-items: center;
+        width: 2.75rem;
+        border-radius: 9999px;
+        height: 1.5rem;
+        background-color: rgb(191, 219, 254, 1);
+
+        &.active {
+            background-color: rgb(37, 99, 235, 1);
+        }
+    }
+`;
+const SwitchLabel = styled.span`
+    margin-right: 1rem;
+`;
+
+const SwitchToggle = styled.span`
+    display: inline-block;
+    width: 1rem;
+    height: 1rem;
+    background-color: #fff;
+    border-radius: 9999px;
+    transform: ${props => (props.toggleOn) ? 'translate(1.5rem, 0)' : 'translate(0, 0)'};
 `;
 
 const Settings = () => {
@@ -32,6 +64,8 @@ const Settings = () => {
     const librarySection = useSelector(state => state.settings.librarySection);
     const resources = useSelector(state => state.settings.servers);
     const libraries = useSelector(state => state.settings.libraries);
+
+    const [enabled, setEnabled] = useState(false);
 
     const serverChanged = (e) => {
         dispatch(setServerSetting(e.target.value));
@@ -51,12 +85,23 @@ const Settings = () => {
                         <option key={resource.clientIdentifier} value={resource.clientIdentifier}>{resource.name}</option>
                     )))}
                 </SelectControl>
+
                 <SelectControl value={librarySection} onChange={libraryChanged}>
                     <option value="" disabled hidden>Select a Library</option>
                     {(libraries.map((library) => (
                         <option key={library.key} value={library.key}>{library.title}</option>
                     )))}
                 </SelectControl>
+
+                <SwitchContainer> 
+                    <SwitchLabel>Dark Mode:</SwitchLabel>
+                    <Switch checked={enabled} onChange={setEnabled}
+                        className={`${enabled ? 'active ' : '' }switch-item`}
+                        >
+                        <SrOnly>Enable notifications</SrOnly>
+                        <SwitchToggle toggleOn={enabled} />
+                    </Switch>
+                </SwitchContainer>
             </ScrollContent>
         </>
     );

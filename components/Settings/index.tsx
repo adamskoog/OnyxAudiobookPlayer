dc895fc8d6444e6025dbc041876e901be18ee0da
adamskoog <adamskoog@outlook.com>
2023-06-25 10:12:46 -0500

    Add initial full page loader.

--- components/Settings/index.tsx
+++ components/Settings/index.tsx
@@ -1,6 +1,7 @@
 import { useAppSelector, useAppDispatch } from '@/store'
 import PlexJavascriptApi from '@/plex'
 
+import { logout } from '@/store/features/applicationSlice';
 import { setActiveServer } from '@/store/features/serverSlice';
 import { setActiveLibrary } from '@/store/features/librarySlice';
 
@@ -41,10 +42,11 @@ function Settings() {
             // https://clients.plex.tv/api/home/users?X-Plex-Product=Plex%20Web&X-Plex-Version=4.108.0&X-Plex-Client-Identifier=pg5y4mmh8eq9gjkah65vt4ue&X-Plex-Platform=Microsoft%20Edge&X-Plex-Platform-Version=114.0&X-Plex-Features=external-media%2Cindirect-media%2Chub-style-list&X-Plex-Model=hosted&X-Plex-Device=Windows&X-Plex-Device-Name=Microsoft%20Edge&X-Plex-Device-Screen-Resolution=1920x937%2C1920x1080&X-Plex-Token=Kqi6dGG2E5rxDyrjYVzh&X-Plex-Language=en&X-Plex-Session-Id=d03b04f5-f957-4a85-81ff-72118a1eb567
         test();
     }
-    const logout = () => {
-        PlexJavascriptApi.logout()
+    const signout = () => {
+        dispatch(logout())
     }
 
+    if (!user) return <></>
     return (
         <div className={`${styles.container}`}>
             {user && (
@@ -53,7 +55,7 @@ function Settings() {
             <Select value={libraryId} onChange={changeLibrary} data={libraryOptions} />
 
             <Button disabled={true} onClick={testing}>{'Switch User'}</Button>
-            <Button onClick={logout}>{'Log out'}</Button>
+            <Button onClick={signout}>{'Log out'}</Button>
             </>
             )}
         </div>

6e01131f23b7d91cac1dbc0ab0a9f1cff870f6fc
adamskoog <adamskoog@outlook.com>
2023-06-25 09:54:43 -0500

    Change display state of album on deck when viewing the current playing album.

--- components/Album/OnDeck.tsx
+++ components/Album/OnDeck.tsx
@@ -1,3 +1,5 @@
+import { Loader } from '@mantine/core';
+
 import { UnstyledButton } from '@mantine/core';
 import { useAppDispatch } from '@/store';
 import { buildPlayQueue } from '@/store/features/playerSlice';
@@ -15,7 +17,7 @@ type OnDeckProps = {
 export default function OnDeck({ album }: OnDeckProps) {
 
     const dispatch = useAppDispatch();
-    const { onDeck } = useOnDeckTrack({ album });
+    const { onDeck, isPlaying } = useOnDeckTrack({ album });
 
     const playTrack = (track: PlexTrack): void => {
         dispatch(buildPlayQueue(getAlbumQueue(track, album)));
@@ -24,12 +26,21 @@ export default function OnDeck({ album }: OnDeckProps) {
     if (!onDeck) return <></>
 
     return (
-        <UnstyledButton className={`${styles.container}`} title={'Play Next Track'} onClick={() => playTrack(onDeck)}>
-            <svg width="1.75rem" height="1.75rem" viewBox="0 0 16 16" className="bi bi-x-circle" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                <path fillRule="evenodd" d="M8 15A7 7 0 1 0 8 1a7 7 0 0 0 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
-                <path fillRule="evenodd" d="m 12.654334,8.697 -6.3630006,3.692 c -0.54,0.313 -1.233,-0.066 -1.233,-0.697 V 4.308 c 0,-0.63 0.692,-1.01 1.233,-0.696 l 6.3630006,3.692 a 0.802,0.802 0 0 1 0,1.393 z"/>
-            </svg>
-            <span>{onDeck.title}</span>
-        </UnstyledButton>
+        <>
+        {isPlaying ? (
+            <div className={`${styles.container}`}>
+                <Loader color="orange" size="xs" variant="bars" />
+                <span>{onDeck.title}</span>
+            </div>
+        ) : (
+            <UnstyledButton className={`${styles.container}`} title={'Play Next Track'} onClick={() => playTrack(onDeck)}>
+                <svg width="1.75rem" height="1.75rem" viewBox="0 0 16 16" className="bi bi-x-circle" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                    <path fillRule="evenodd" d="M8 15A7 7 0 1 0 8 1a7 7 0 0 0 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+                    <path fillRule="evenodd" d="m 12.654334,8.697 -6.3630006,3.692 c -0.54,0.313 -1.233,-0.066 -1.233,-0.697 V 4.308 c 0,-0.63 0.692,-1.01 1.233,-0.696 l 6.3630006,3.692 a 0.802,0.802 0 0 1 0,1.393 z"/>
+                </svg>
+                <span>{onDeck.title}</span>
+            </UnstyledButton>
+        )}
+        </>
     )
 }

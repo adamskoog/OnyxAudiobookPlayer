a3ece18bc8b136052292a0615c99f6453ce1da2c
adamskoog <adamskoog@outlook.com>
2023-06-27 13:10:01 -0500

    Complete Plex Home user switching. Add display for app version in settings. Bug fixes to auth flow.

--- pages/settings.tsx
+++ pages/settings.tsx
@@ -1,8 +1,19 @@
+import type { InferGetServerSidePropsType, GetServerSideProps } from 'next'
+
 import Head from 'next/head'
 
 import SettingsPage from '@/components/Settings'
 
-export default function Settings() {
+import pjson from '@/package.json'
+
+export const getServerSideProps: GetServerSideProps<{ version: string }> = async () => {
+    // TODO: not sure we want this - we also have a version within PlexJavascriptApi
+    // which needs to be updated if using the one from package.json. The whole point
+    // is to not have to update it in multiple places....
+    return { props: { version: pjson.version }}
+}
+
+export default function Page({ version }: InferGetServerSidePropsType<typeof getServerSideProps>) {
   return (
     <>
       <Head>
@@ -11,7 +22,7 @@ export default function Settings() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <SettingsPage />
+      <SettingsPage appVersion={version} />
     </>
   )
 }

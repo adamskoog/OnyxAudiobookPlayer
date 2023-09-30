a32516c31a21f2a4aa30a0a2111e3657c1c66136
adamskoog <adamskoog@outlook.com>
2023-09-29 23:20:02 -0500

    Refactor some items to be in the FetchInstance class as helpers.

--- utility/FetchInstance.ts
+++ utility/FetchInstance.ts
@@ -1,56 +1,117 @@
+import qs from 'qs';
+
+type GetOptions = {
+    params?: any,
+    headers?: any
+}
+
+type PostOptions = {
+  params?: any,
+  body? :any,
+  headers?: any
+}
 class FetchInstance {
     private baseUrl: string | null = null;
 
     constructor(rootUrl: string) {
-        //TODO: string ending /
+        if (rootUrl.endsWith('/')) rootUrl = rootUrl.substring(0, rootUrl.length - 1);
         this.baseUrl = rootUrl;
     }
 
-    public async get(url: string, params: any = null, headers: any = {}): Promise<any> {
-      let getParams = '';
-      if (params) getParams = `?${new URLSearchParams(params)}`;
-        const response = await fetch(`${this.baseUrl}${url}${getParams}`, {
-            method: 'GET',
-            headers: {
-              'Accept': 'application/json',
-              'Content-type': 'application/json',
+    private mergeHeaders(headers: any, prevHeaders?: any): any {
+
+        if (prevHeaders) {
+            return {
+                ...prevHeaders,
                 ...headers
             }
-        });
+        }
 
+        return headers;
+    }
+
+    public async get(url: string, options?: GetOptions): Promise<any> {
+      let getParams = '';
+      if (options?.params) getParams = `?${new URLSearchParams(options.params)}`;
+
+      return await fetch(`${this.baseUrl}${url}${getParams}`, {
+          method: 'GET',
+          headers: options?.headers ?? {}
+      });
+    }
+
+    public async getJson(url: string, options?: GetOptions): Promise<any> {
+
+        let internalHeaders = {
+          'Accept': 'application/json',
+          'Content-type': 'application/json',
+        };
+
+        if (!options) options = { headers: internalHeaders};
+        else options.headers = this.mergeHeaders(internalHeaders, options.headers);
+
+        const response = await this.get(url, options);
         return await response.json();
     }
 
-    public async getXml(url: string, params: any = null, headers: any = {}): Promise<any> {
-        const getParams = new URLSearchParams(params);
-        const response = await fetch(`${this.baseUrl}${url}?${getParams}`, {
-            method: 'GET',
-            headers: {
-                'Accept': 'application/xml',
-                'Content-type': 'application/xml',
-                ...headers
-            }
-          });
+    public async getXml(url: string, options?: GetOptions): Promise<any> {
 
+        let internalHeaders = {
+          'Accept': 'application/xml',
+          'Content-type': 'application/xml',
+        };
+
+        if (!options) options = { headers: internalHeaders};
+        else options.headers = this.mergeHeaders(internalHeaders, options.headers);
+
+        const response = await this.get(url, options);
         return await response.text();
     }
 
-    public async post(url: string, data: any, params: any, headers: any = {}): Promise<any> {
+    public async post(url: string, options?: PostOptions): Promise<any> {
         let getParams = '';
-        if (params) getParams = `?${new URLSearchParams(params)}`;
+        if (options?.params) getParams = `?${new URLSearchParams(options.params)}`;
 
-        const response = await fetch(`${this.baseUrl}${url}${getParams}`, {
+        return await fetch(`${this.baseUrl}${url}${getParams}`, {
           method: 'POST',
-          headers: {
-            'Accept': 'application/json',
-            'Content-type': 'application/json',
-              ...headers
-          },
-          body: JSON.stringify(data ?? {})
+          headers: options?.headers ?? {},
+          body: JSON.stringify(options?.body ?? {})
         });
+    }
 
+    public async postJson(url: string, options?: PostOptions): Promise<any> {
+        let internalHeaders = {
+          'Accept': 'application/json',
+          'Content-type': 'application/json',
+        };
+
+        if (!options) options = { headers: internalHeaders};
+        else options.headers = this.mergeHeaders(internalHeaders, options.headers);
+
+        const response = await this.post(url, options);
         return await response.json();
     }
+
+    public static async fetchWithTimeout(url: string, options: any): Promise<any> {
+      const { timeout = 8000 } = options;
+  
+      const controller = new AbortController();
+      const id = setTimeout(() => controller.abort(), timeout);
+    
+      const response = await fetch(url, {
+        ...options,
+        signal: controller.signal,
+      });
+      clearTimeout(id);
+    
+      return response;
+    };
+
+    public static formatUrl (url: string, args: any): string {
+        const params = qs.stringify(args);
+        if (params && params !== '') return `${url}?${params}`;
+        return url;
+    };
 }
 
 export default FetchInstance;

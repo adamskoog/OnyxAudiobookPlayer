87a9dd55496629f6c93ac0e5e3ac54e9aad88504
adamskoog <adamskoog@outlook.com>
2023-11-11 13:11:19 -0600

    Change getting version to getstaticprops.

--- pages/settings.tsx
+++ pages/settings.tsx
@@ -1,4 +1,4 @@
-import type { InferGetServerSidePropsType, GetServerSideProps } from 'next'
+import type { InferGetStaticPropsType, GetStaticProps } from 'next'
 
 import Head from 'next/head'
 
@@ -6,14 +6,14 @@ import SettingsPage from '@/components/Settings'
 
 import pjson from '@/package.json'
 
-export const getServerSideProps: GetServerSideProps<{ version: string }> = async () => {
+export const getStaticProps: GetStaticProps<{ version: string }> = async () => {
     // TODO: not sure we want this - we also have a version within PlexJavascriptApi
     // which needs to be updated if using the one from package.json. The whole point
     // is to not have to update it in multiple places....
     return { props: { version: pjson.version }}
 }
 
-export default function Page({ version }: InferGetServerSidePropsType<typeof getServerSideProps>) {
+export default function Page({ version }: InferGetStaticPropsType<typeof getStaticProps>) {
   return (
     <>
       <Head>

5d7d32d315c5dafa6f1abaf9919826bddb0231a3
adamskoog <adamskoog@outlook.com>
2023-12-29 14:45:55 -0600

    Update version.

--- plex/index.ts
+++ plex/index.ts
@@ -45,7 +45,7 @@ class PlexJavascriptApi {
 
     private static requestBaseParams: any = {
         'X-Plex-Product': 'Onyx Audiobook Player',
-        'X-Plex-Version': '2.4.1',
+        'X-Plex-Version': '2.5.0',
         'X-Plex-Client-Identifier': null
     };
 
